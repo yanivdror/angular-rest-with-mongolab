b758298c508b3ec3a917c0a2b82aa85b2b6b2b62
yanivdror <yanivdror25@gmail.com>
2015-08-25 13:49:39 +0300

    improving add $q

--- app.js
+++ app.js
@@ -1,13 +1,92 @@
 	(function() {
+
+//Globals
+
 		var name = "myApp",
 			requires = [],
 			myApp = null,
 			url ="https://api.mongolab.com/api/1/databases/my-academy/collections/course",
 			config = {params:{apiKey:"8w19XWoI1u9evDYqoWwR1on4TebbD_mo"}};
+			
 		
 		myApp = angular.module(name, requires);
-		
-		myApp.controller("AppCtrl", function($scope,$http) {
+
+//START service		
+
+		myApp.factory("courseDataSvc", function($http,$q){
+
+			function qDeferInit($q){
+				return $q.defer();
+			}			
+			
+
+			return {
+
+				getCourses:function(){
+
+					var deferred = qDeferInit($q);				
+
+					$http.get(url,config)
+						.success(function(data){
+							deferred.resolve(data);								
+						})
+						.error(function(err){
+							deferred.reject(err);							
+						});
+
+					return deferred.promise;	
+				},
+				addCourse:function(course){
+
+					var deferred = qDeferInit($q);						
+
+					$http.post(url,course,config)
+						.success(function(data){
+							deferred.resolve(data);								
+						})
+						.error(function(err){
+							deferred.reject(err);							
+						});
+
+					return deferred.promise;										
+				},
+				updateCourse:function(courseToEdit){
+
+					var deferred = qDeferInit($q);
+					var id = courseToEdit._id.$oid;
+
+					$http.put(url+"/"+id,courseToEdit,config)
+						.success(function(data){
+							deferred.resolve(data);								
+						})
+						.error(function(err){
+							deferred.reject(err);							
+						});
+
+					return deferred.promise;											
+				},
+				deleteCourse:function(course){
+
+					var deferred = qDeferInit($q);
+					var id = course._id.$oid;
+
+				$http.delete(url+"/"+id,config)
+						.success(function(data){
+							deferred.resolve(data);								
+						})
+						.error(function(err){
+							deferred.reject(err);							
+						});
+
+					return deferred.promise;					
+				}
+			}
+		});
+//END service
+
+//START controller
+
+		myApp.controller("AppCtrl", function($scope,$http,courseDataSvc) {
 		    $scope.courses = [];
 			
 			// variables for toggling view states
@@ -17,45 +96,61 @@
 			// to load all courses
 			$scope.loadCourses = function() {
 
-				$http.get(url,config)
-					.success(function(data){
-						$scope.courses = data;
-					});
+				courseDataSvc.getCourses().then(
+
+				function (data){
+					$scope.courses = data;
+				},
+				function error(e){
+					console.log("Error "+e);
+				});
+
+
+
 			}
 
 			$scope.addCourse = function(course){
-				$http.post(url,course,config)
-					.success(function(data){
+
+				courseDataSvc.addCourse(course).then(
+
+					function(data){
 						console.log("added successfully "+JSON.stringify(data));
 						$scope.loadCourses();
-					});
-			}
-
-			$scope.editCourse = function(course){
-				$scope.toggleEditCourseView = true;
-				$scope.courseToEdit = angular.copy(course);
+					},
+					function error(e){
+					console.log("Error "+e);
+				});
 			}
 
 			$scope.updateCourse = function(courseToEdit){
 
-				var id = courseToEdit._id.$oid;
+				courseDataSvc.updateCourse(courseToEdit).then(
 
-				$http.put(url+"/"+id,courseToEdit,config)
-					.success(function(data){
+					function(data){
 						console.log("updated successfully "+JSON.stringify(data));
 						$scope.loadCourses();
-					});
+					},
+					function error(e){
+					console.log("Error "+e);
+				});
 			}
 
 			$scope.deleteCourse = function(course){
 
-				var id = course._id.$oid;
+				courseDataSvc.deleteCourse(course).then(
 
-				$http.delete(url+"/"+id,config)
-					.success(function(data){
-						console.log("deleted successfully "+JSON.stringify(data));
+					function(data){
+						//console.log("deleted successfully "+JSON.stringify(data));
 						$scope.loadCourses();
-					});
+					},
+					function error(e){
+					console.log("Error "+e);
+				});
+			}			
+
+			$scope.editCourse = function(course){
+				$scope.toggleEditCourseView = true;
+				$scope.courseToEdit = angular.copy(course);
 			}
 			
 			$scope.toggleAddCourse = function(flag) {
@@ -66,4 +161,9 @@
 				$scope.toggleEditCourseView = flag;
 			}
 		});
+
+//END controller
+
+
+
 	}());
